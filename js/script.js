a7b72783f100ae74f6cbd6869bf7f26055b557c2
“SvnFrs” <thaidvqce170410@fpt.edu.vn>
2023-10-10 14:07:32 +0700

    Update modal

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('#reservebtn').click(function(){
+      $('#reserveModal').modal('show');
+    });
+    $('#login').click(function(){
+        $('#loginModal').modal('show');
+      });
+  });
